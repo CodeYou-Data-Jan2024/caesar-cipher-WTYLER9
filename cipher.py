4b0f2940850cb3d69f2c73125c34245e4f089f31
WTYLER9 <wtyler9889@gmail.com>
2024-03-27 04:32:30 -0400

    Cipher Exercise Completed

--- cipher.py
+++ cipher.py
@@ -1,1 +1,14 @@
-# add your code here
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
+shift = 5
+encrypted = []
+li = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+for i in sentence:
+    if i in li:
+        index = (li.index(i) + shift)%26
+        encrypted.append(li[index])
+    else:
+        encrypted.append(i)
+
+print("The encrypted sentence is:", "".join(encrypted))
